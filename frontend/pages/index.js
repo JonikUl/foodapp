2215bec6651d242f446511596c730787cb29703b
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2020-10-17 08:05:25 +0400

    initial
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

---
+++ frontend/pages/index.js
@@ -0,0 +1,16 @@
+import {Button, Alert} from 'reactstrap';
+
+export default () => {
+  return (
+    <div>
+      <div>
+        <Alert color='primary'>
+          Hello Project
+        </Alert>
+        <Button color='primary'>nextjs</Button>
+      </div>
+    </div>
+  )
+}
+
+
---
+++ frontend/pages/register.js
@@ -0,0 +1,3 @@
+export default () => {
+  return <h1>Sign Up</h1>
+}
